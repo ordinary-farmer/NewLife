740c5949ade73d0043ed3b66a41d9b5557c33e5b
PARK JOO HYEONG <ordy.farmer2@gmail.com>
2021-08-06 07:56:23 +0900

    Add GameMode: GameplayGameMode

---
+++ Source/NewLife/Public/GameMode/NLGameMode_GamePlay.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NLGameMode_GamePlay.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEWLIFE_API ANLGameMode_GamePlay : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
