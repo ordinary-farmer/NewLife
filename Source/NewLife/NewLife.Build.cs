bc0d2b6281ac52d5745ba7e8a3185a6b8c3601c1
PARK JOO HYEONG <ordy.farmer2@gmail.com>
2021-08-06 07:37:35 +0900

    Initialize Project

---
+++ Source/NewLife/NewLife.Build.cs
@@ -0,0 +1,23 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class NewLife : ModuleRules
+{
+	public NewLife(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Slate", "SlateCore", "UMG", "GameplayTags" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/NewLife/NewLifeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NewLifeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEWLIFE_API ANewLifeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
