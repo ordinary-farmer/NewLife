2083513b345d2c8949c77b3770aa0a335a0d68b3
PARK JOO HYEONG <ordy.farmer2@gmail.com>
2021-08-06 09:59:23 +0900

    Add Character Stat Structures

---
+++ Source/NewLife/Public/Common/FloatStat.h
@@ -0,0 +1,62 @@
+﻿// Made By Park Joo Hyeong. This is my first Portfollio.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FloatStat.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnCurrentFloatStatChangedSignature);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnCurrentFloatStatUnderMinSignature);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnMaxFloatStatChangedSignature);
+
+USTRUCT(BlueprintType)
+struct FFloatStat
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	float Current;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	float Min;
+
+	UPROPERTY(BlueprintReadOnly)
+	float MinOfMax;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	float Max;
+
+public:
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "Event Dispatcher")
+	FOnCurrentFloatStatChangedSignature OnCurrentChanged;
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "Event Dispatcher")
+	FOnCurrentFloatStatUnderMinSignature OnCurrentUnderMin;
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "Event Dispatcher")
+	FOnMaxFloatStatChangedSignature OnMaxChanged;
+	
+	FFloatStat();
+	FFloatStat(float DefaultCurrent, float DefaultMax, float DefaultMin = 0.f);
+	virtual ~FFloatStat();
+
+	void SetData(float NewCurrent, float NewMax);
+
+	void IncreaseCurrent(float Value);
+
+	void DecreaseCurrent(float Value);
+
+	void IncreaseMax(float Value);
+
+	void DecreaseMax(float Value);
+
+	FORCEINLINE float GetCurrent() const { return Current; }
+
+	FORCEINLINE float GetMin() const { return Min; }
+
+	FORCEINLINE float GetMax() const { return Max; }
+};
+
---
+++ Source/NewLife/Public/Common/Int32Stat.h
@@ -0,0 +1,61 @@
+﻿// Made By Park Joo Hyeong. This is my first Portfollio.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Int32Stat.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnCurrentInt32StatChangedSignature);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnCurrentInt32StatUnderMinSignature);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnMaxInt32StatChangedSignature);
+
+USTRUCT(BlueprintType)
+struct FInt32Stat
+{
+	GENERATED_BODY()
+
+protected:	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	int32 Current;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	int32 Min;
+
+	UPROPERTY(BlueprintReadOnly)
+	int32 MinOfMax;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	int32 Max;
+
+public:
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "Event Dispatcher")
+	FOnCurrentInt32StatChangedSignature OnCurrentChanged;
+	
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "Event Dispatcher")
+	FOnCurrentInt32StatUnderMinSignature OnCurrentUnderMin;
+	
+	UPROPERTY(BlueprintAssignable, BlueprintCallable, Category = "Event Dispatcher")
+	FOnMaxInt32StatChangedSignature OnMaxChanged;
+	
+	FInt32Stat();
+	FInt32Stat(int32 DefaultCurrent, int32 DefaultMax, int32 DefaultMin = 0);
+	virtual ~FInt32Stat();
+
+	void SetData(int32 NewCurrent, int32 NewMax);
+
+	void IncreaseCurrent(int32 Value);
+
+	void DecreaseCurrent(int32 Value);
+
+	void IncreaseMax(int32 Value);
+
+	void DecreaseMax(int32 Value);
+
+	FORCEINLINE int32 GetCurrent() const { return Current; }
+
+	FORCEINLINE int32 GetMin() const { return Min; }
+
+	FORCEINLINE int32 GetMax() const { return Max; }
+};
